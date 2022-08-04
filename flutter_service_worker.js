f498664f88cf14453ed2fa2958bc3d43eb1c1760
Brian Quan <qxy1004@gmail.com>
2022-08-04 12:33:35 +0100

    Add on hold feature

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "db26f83aa2dd81e17060183e016f3064",
-"index.html": "df0f36d710a5d14bc8638db94d6fddf3",
-"/": "df0f36d710a5d14bc8638db94d6fddf3",
-"main.dart.js": "55225e3e87ea6e497d1d75f28518797b",
+"index.html": "62ad8a835fbcf578b6608d00944b403f",
+"/": "62ad8a835fbcf578b6608d00944b403f",
+"main.dart.js": "b75c8af0a5c56a63b2601d5e0329ea1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
@@ -14,7 +14,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "3048593c2c43317ad1fff7af220d87fe",
 "assets/AssetManifest.json": "dbeb7da9302ece998ce63acf0edd51ce",
-"assets/NOTICES": "bd3b007f61445f2e8b8168fd8eb4334f",
+"assets/NOTICES": "90cec802944237fbe551e562155614ab",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/packages/fluttertoast/assets/toastify.js": "e7006a0a033d834ef9414d48db3be6fc",

ac6951917b9777be19faa86c8d54680768693081
Brian Quan <qxy1004@gmail.com>
2023-01-04 15:35:49 +0000

    Improve how to get target to reduce API call

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "db26f83aa2dd81e17060183e016f3064",
-"index.html": "8f5bb90a6a43f7e2e99486d696075642",
-"/": "8f5bb90a6a43f7e2e99486d696075642",
-"main.dart.js": "c2857a664269adf4224b6dca085b3ea5",
+"index.html": "42825c80f1d6ce88a102a6d76c47679d",
+"/": "42825c80f1d6ce88a102a6d76c47679d",
+"main.dart.js": "5990007235482d4a080a44d1ec28f5cd",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "favicon.png": "9fe9f0e78d9eee9a2e2ec5700dd8eb32",
 "icons/Icon-192.png": "f3782d20718cb39ee1261164e7e2d5c1",
